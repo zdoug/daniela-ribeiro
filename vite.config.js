079d9f8f134c3772f44e9b2f13b3f6599992249b
Douglas Oliveira <ydougx@gmail.com>
2026-04-12 17:02:21 -0300

    Add GitHub Actions workflow for deploy

---
+++ vite.config.js
@@ -0,0 +1,7 @@
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  // Isso garante que o Vite gere caminhos relativos corretos
+  // para o seu domínio próprio.
+  base: '/', 
+})
